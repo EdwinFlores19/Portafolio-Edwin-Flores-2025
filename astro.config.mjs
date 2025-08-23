5f0cb60148d6a3f805af0a55b7671b34d6293887
Edwin Flores <82065912+EdwinFlores19@users.noreply.github.com>
2025-08-23 03:19:46 -0500

    Configure site and base path for GitHub Pages v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
-
-// Importa las integraciones que estás utilizando
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  // Configuración específica para el despliegue en GitHub Pages
+  // URL base del sitio donde se desplegará
   site: 'https://edwinflores19.github.io',
+  // Subdirectorio donde se encuentra el proyecto
   base: '/Portafolio-Edwin-Flores-2025/',
-
-  // Lista de todas las integraciones que tu proyecto necesita
+  
+  // Integraciones de Astro
   integrations: [
     tailwind(), 
     react()
